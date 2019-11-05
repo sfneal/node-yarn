a5e4708e4b886431d4369a6b25c1bb8a578eb7e2
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:35:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/node-yarn:v1 ./v1/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/node-yarn:v1 "${DIR}"/v1/
