e95fda839a91f7c989abea63059ed3d36b705f3e
mrstephenneal <stephen.neal14@gmail.com>
2020-02-06 12:52:09 -0500

    MAKE v2 image that uses a newer version of yarn by installing via NPM

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -t stephenneal/node-yarn:v1 "${DIR}"/v1/
+docker build -t stephenneal/node-yarn:v2 "${DIR}"/v2/
--- push.sh
+++ push.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sh "${DIR}"/build.sh
 
 docker push stephenneal/node-yarn:v1
+docker push stephenneal/node-yarn:v2
