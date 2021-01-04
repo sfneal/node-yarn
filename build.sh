33f241254a5357c889c75e585de035f105644635
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:23:55 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/node-yarn:v1 "${DIR}"/v1/
-docker build -t stephenneal/node-yarn:v2 "${DIR}"/v2/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/node-yarn:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/node-yarn:v1 "${DIR}"/v1/
+    docker build -t stephenneal/node-yarn:v2 "${DIR}"/v2/
+fi
